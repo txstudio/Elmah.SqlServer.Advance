48564d03f852ee580feb2ec1b26deb7f2c41dd6c
txstudio <st890409@hotmail.com>
2015-02-26 15:25:55 +0800

    建立相關資料庫物件
    建立 ElmahError 應用程式錯誤的資料庫物件
    - 單/雙 數月份資料表
    - 儲存錯誤資料的預存程序 (StoredProcedure)

---
+++ elmah.advance.sql
@@ -0,0 +1,107 @@
+﻿--儲存雙數月份的 Elmah 應用程式執行錯誤
+CREATE TABLE [dbo].[ElmahError-0] (
+	[No] INT IDENTITY(1, 1) NOT NULL
+	,[Application] NVARCHAR(60)
+	,[Host] NVARCHAR(50)
+	,[Type] NVARCHAR(100)
+	,[Source] NVARCHAR(60)
+	,[Message] NVARCHAR(500)
+	,[User] NVARCHAR(50)
+	,[StatusCode] INT
+	,[TimeUtc] DATETIME DEFAULT(GETDATE())
+	,[AllXml] NVARCHAR(MAX)
+	,CONSTRAINT [PK_dbo_ElmahError-0_No] PRIMARY KEY CLUSTERED ([No] ASC) ON [PRIMARY]
+	) ON [PRIMARY] TEXTIMAGE_ON [TEXTIMAGE]
+GO
+
+--儲存單數月份的 Elmah 應用程式執行錯誤
+CREATE TABLE [dbo].[ElmahError-1] (
+	[No] INT IDENTITY(1, 1) NOT NULL
+	,[Application] NVARCHAR(60)
+	,[Host] NVARCHAR(50)
+	,[Type] NVARCHAR(100)
+	,[Source] NVARCHAR(60)
+	,[Message] NVARCHAR(500)
+	,[User] NVARCHAR(50)
+	,[StatusCode] INT
+	,[TimeUtc] DATETIME DEFAULT(GETDATE())
+	,[AllXml] NVARCHAR(MAX)
+	,CONSTRAINT [PK_dbo_ElmahError-1_No] PRIMARY KEY CLUSTERED ([No] ASC) ON [PRIMARY]
+	) ON [PRIMARY] TEXTIMAGE_ON [TEXTIMAGE]
+GO
+
+--
+--儲存 Elmah 應用程式執行錯誤的使用者自訂預存程序內容
+--
+CREATE PROCEDURE [dbo].[ELMAH_LogError] (
+	@ErrorId UNIQUEIDENTIFIER
+	,@Application NVARCHAR(60)
+	,@Host NVARCHAR(30)
+	,@Type NVARCHAR(100)
+	,@Source NVARCHAR(60)
+	,@Message NVARCHAR(500)
+	,@User NVARCHAR(50)
+	,@AllXml NVARCHAR(MAX)
+	,@StatusCode INT
+	,@TimeUtc DATETIME
+	)
+AS
+SET NOCOUNT ON
+SET @TimeUtc = (GETDATE())
+
+--判斷資料要被儲存在那一個資料表
+IF (DATEPART(MONTH, @TimeUtc) % 2) = 0
+BEGIN
+	INSERT INTO [dbo].[ElmahError-0] (
+		--[ErrorId],
+		[Application]
+		,[Host]
+		,[Type]
+		,[Source]
+		,[Message]
+		,[User]
+		,[AllXml]
+		,[StatusCode]
+		,[TimeUtc]
+		)
+	VALUES (
+		--@ErrorId,
+		@Application
+		,@Host
+		,@Type
+		,@Source
+		,@Message
+		,@User
+		,@AllXml
+		,@StatusCode
+		,@TimeUtc
+		)
+END
+ELSE
+BEGIN
+	INSERT INTO [dbo].[ElmahError-1] (
+		--[ErrorId],
+		[Application]
+		,[Host]
+		,[Type]
+		,[Source]
+		,[Message]
+		,[User]
+		,[AllXml]
+		,[StatusCode]
+		,[TimeUtc]
+		)
+	VALUES (
+		--@ErrorId,
+		@Application
+		,@Host
+		,@Type
+		,@Source
+		,@Message
+		,@User
+		,@AllXml
+		,@StatusCode
+		,@TimeUtc
+		)
+END
+GO
